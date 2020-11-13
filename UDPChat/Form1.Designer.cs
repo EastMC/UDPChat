4120a3bcd37c8073b8d1dceb5fbc8f0af1b10106
Максим Сергеевич <afrika_rulit@mail.ru>
2020-11-13 11:22:47 +0300

    Login form + ini reader

--- UDPChat/Form1.Designer.cs
+++ UDPChat/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UDPChat
 {
-    partial class Form1
+    partial class FormStartup
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,23 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ButtonEnter = new System.Windows.Forms.Button();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.TextBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // ButtonEnter
+            // 
+            this.ButtonEnter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonEnter.Location = new System.Drawing.Point(152, 82);
+            this.ButtonEnter.Name = "ButtonEnter";
+            this.ButtonEnter.Size = new System.Drawing.Size(75, 23);
+            this.ButtonEnter.TabIndex = 3;
+            this.ButtonEnter.Text = "Вход";
+            this.ButtonEnter.UseVisualStyleBackColor = true;
+            this.ButtonEnter.Click += new System.EventHandler(this.ButtonEnter_Click);
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLogin.Location = new System.Drawing.Point(85, 12);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(140, 26);
+            this.textBoxLogin.TabIndex = 1;
+            // 
+            // TextBoxPassword
+            // 
+            this.TextBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBoxPassword.Location = new System.Drawing.Point(85, 44);
+            this.TextBoxPassword.Name = "TextBoxPassword";
+            this.TextBoxPassword.PasswordChar = '*';
+            this.TextBoxPassword.Size = new System.Drawing.Size(140, 26);
+            this.TextBoxPassword.TabIndex = 2;
+            this.TextBoxPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxPassword_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Пароль";
+            // 
+            // FormStartup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(422, 341);
+            this.ClientSize = new System.Drawing.Size(239, 117);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.ButtonEnter);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "Form1";
+            this.Name = "FormStartup";
             this.ShowInTaskbar = false;
+            this.Text = "UDP";
+            this.Load += new System.EventHandler(this.FormStartup_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ButtonEnter;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox TextBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- UDPChat/Form1.cs
+++ UDPChat/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,43 @@ using System.Windows.Forms;
 
 namespace UDPChat
 {
-    public partial class Form1 : Form
+    public partial class FormStartup : Form
     {
-        public Form1()
+        private INIManager iniManager = new INIManager(Directory.GetCurrentDirectory() + "\\settings.ini");
+
+        public FormStartup()
         {
             InitializeComponent();
+        }
+
+        private void ButtonEnter_Click(object sender, EventArgs e)
+        {
+            if (textBoxLogin.Text.Any() && TextBoxPassword.Text.Any())
+            {
+                iniManager.WritePrivateString("credentials", "login", textBoxLogin.Text);
+                this.Close();
+            }
+            else MessageBox.Show("Введите логин и пароль");
+
+        }
+
+        private void FormStartup_Load(object sender, EventArgs e)
+        {
+            string login = iniManager.GetPrivateString("credentials", "login");
+            textBoxLogin.Text = login;
+
+            if (textBoxLogin.Text.Any())
+            {
+                TextBoxPassword.Select();
+            }
+        }
+
+        private void TextBoxPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                ButtonEnter_Click(this, new EventArgs());
+            }
         }
     }
 }
